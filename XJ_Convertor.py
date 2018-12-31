3fd812d28bed76c8ad0ba3170ead4ec01e6fd49e
moustaphboss <mustafakebe99@gmail.com>
2018-12-31 13:26:01 +0000

    Add files via upload

--- XJ_Convertor.py
+++ XJ_Convertor.py
@@ -5,53 +5,59 @@ import validation
 from pathlib import Path
 from glob import glob
 
-# DEBUT Fonction permettant d'extraire les donnees du fichier json
+# DEBUT Fonction permettant d'extraire les donnees du fichier json en respectant le format
 def parseJSON(filePath):
     file = open(filePath)
     data = file.read()
     j = json.loads(data)
     return j
-# FIN Fonction permettant d'extraire les donnees du fichier json
+# FIN Fonction permettant d'extraire les donnees du fichier json en respectant le format
 
 
-fileType = sys.argv[2]
-inputType = sys.argv[3]
-myfile = sys.argv[4]
-fileName, fileExt = os.path.splitext(myfile)
-fileExt = fileExt.lower()
+if len(sys.argv) == 7:
 
-if __name__ == "__main__":
-    # Verification si le fichier en entree existe
-    if not(os.path.exists(myfile)):
-        print('---Erreur: Ce fichier n\'existe pas !')
-    else:
-        if inputType == '-f':
-            file = open(myfile)
-            data = file.read()
-            if fileType == 'json':
-                # Verification de l'extention du fichier en input
-                if (fileExt != '.json'):
-                    print('---Erreur: Veuillez entrer un fichier JSON')
-                else:
-                    if validation.json_validator(data):
-                        
-                        import create_svg
-                    else:
-                        print(validation.json_validator(data))
+    fileType = sys.argv[2]
+    inputType = sys.argv[3]
+    myfile = sys.argv[4]
+    svgFile = sys.argv[6]
 
-            elif fileType == "xml":
-                if (fileExt != '.xml'):
-                    print('---Erreur: Veuillez entrer un fichier XML')
-                else:
-                    if validation.xml_validator(myfile):
-                        print('Traitement du fichier XML')
-                    else:
-                        validation.xml_validator(myfile)
-            else:
-                print("---Erreur: Veuillez specifier un type de fichier correct.")
-                print("---Votre choix: "+fileType)
-        elif inputType == '-h':
-            print('Methode http')
+    fileName, fileExt = os.path.splitext(myfile)
+    fileExt = fileExt.lower()
+
+    if __name__ == "__main__":
+        # Verification si le fichier en entree existe
+        if not(os.path.exists(myfile)):
+            print('---Erreur: Ce fichier n\'existe pas !')
         else:
-            print("---Erreur: Veuillez specifier un type d'acquisition de fichier correct.")
-            print("---Votre choix: "+inputType)
+            if inputType == '-f':
+                file = open(myfile)
+                data = file.read()
+                if fileType == 'json':
+                    # Verification de l'extention du fichier en input
+                    if (fileExt != '.json'):
+                        print('---Erreur: Veuillez entrer un fichier JSON')
+                    else:
+                        if validation.json_validator(data):
+
+                            import create_svg
+                        else:
+                            print(validation.json_validator(data))
+
+                elif fileType == "xml":
+                    if (fileExt != '.xml'):
+                        print('---Erreur: Veuillez entrer un fichier XML')
+                    else:
+                        if validation.xml_validator(myfile):
+                            print('Traitement du fichier XML')
+                        else:
+                            validation.xml_validator(myfile)
+                else:
+                    print("---Erreur: Veuillez specifier un type de fichier correct.")
+                    print("---Votre choix: "+fileType)
+            elif inputType == '-h':
+                print('Methode http')
+            else:
+                print("---Erreur: Veuillez specifier un type d'acquisition de fichier correct.")
+                print("---Votre choix: "+inputType)
+else:
+    print('---Erreur: Nombre d\'arguments incorrect')
--- create_svg.py
+++ create_svg.py
@@ -11,7 +11,7 @@ for i in range(len(myJsonData)):
         listEntites.append(key)
 
 # Initiation du fichier SVG
-svg_document = svgwrite.Drawing(filename = "test-svgwrite.svg",
+svg_document = svgwrite.Drawing(filename = XJ_Convertor.svgFile,
                                 debug = True)
 
 # Recuperation des noms des entités que l'on enregistre dans la variable "listEntites"
@@ -26,7 +26,13 @@ for i in range(len(listEntites)):
     if (i % 2 == 0):
         # Creation du rectangle contenant les infos de l'entité
         svg_document.add(svg_document.rect(insert = (10, i*150 + 10),
-                                           size = ("150px", "100px"),
+                                           size = ("150px", "130px"),
+                                           stroke_width = "1",
+                                           stroke = "black",
+                                           fill = "rgb(209, 250, 46)"))
+
+        svg_document.add(svg_document.rect(insert = (10, i*150 + 10),
+                                           size = ("150px", "26px"),
                                            stroke_width = "1",
                                            stroke = "black",
                                            fill = "rgb(209, 250, 46)"))
@@ -38,11 +44,17 @@ for i in range(len(listEntites)):
         # Affichage de la liste des attributs
         for k in range(len(listAttributs)):
             svg_document.add(svg_document.text(listAttributs[k],
-                                               insert = (20, i*150 + 30 + (k+1)*20)))
+                                               insert = (20, i*150 + 40 + (k+1)*20)))
     else:
         # Creation du rectangle contenant les infos de l'entité
         svg_document.add(svg_document.rect(insert = (400, (i - 1)*150 + 10),
-                                           size = ("150px", "100px"),
+                                           size = ("150px", "130px"),
+                                           stroke_width = "1",
+                                           stroke = "black",
+                                           fill = "rgb(209, 250, 46)"))
+
+        svg_document.add(svg_document.rect(insert = (400, (i - 1)*150 + 10),
+                                           size = ("150px", "26px"),
                                            stroke_width = "1",
                                            stroke = "black",
                                            fill = "rgb(209, 250, 46)"))
@@ -54,7 +66,7 @@ for i in range(len(listEntites)):
         # Affichage de la liste des attributs
         for k in range(len(listAttributs)):
             svg_document.add(svg_document.text(listAttributs[k],
-                                               insert = (410, (i - 1)*150 + 30 + (k+1)*20)))
+                                               insert = (410, (i - 1)*150 + 40 + (k+1)*20)))
 
 # ---------------
 
