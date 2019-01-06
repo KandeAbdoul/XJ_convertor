b45f94f833625bd3f7a866af94a08db6867b7cba
moustaphboss <mustafakebe99@gmail.com>
2019-01-06 17:02:33 +0000

    Add files via upload

--- create_svg.py
+++ create_svg.py
@@ -6,23 +6,32 @@ myJsonData = XJ_Convertor.parseJSON(XJ_Convertor.myfile)
 
 listEntites = []
 # Recuperation de la liste des entités
+
 for i in range(len(myJsonData)):
     for key in myJsonData[i].keys():
         listEntites.append(key)
+
 
 # Initiation du fichier SVG
 svg_document = svgwrite.Drawing(filename = XJ_Convertor.svgFile,
                                 debug = True)
 
+entityCoordsList = []
 # Recuperation des noms des entités que l'on enregistre dans la variable "listEntites"
+print('Liste des entites et leurs attributs: ')
 for i in range(len(listEntites)):
     listAttributs = []
+    listAssocs = []
+
     # Recuperation des noms des attributs que l'on enregistre dans la variable "listAttributs"
     nbAttributs = len(myJsonData[i][listEntites[i]]['attributs'])
+    print('\t' + listEntites[i])
     for j in range(nbAttributs):
         for key in myJsonData[i][listEntites[i]]['attributs'][j].keys():
             listAttributs.append(key)
+            print('\t\t' + key)
 
+    
     if (i % 2 == 0):
         # Creation du rectangle contenant les infos de l'entité
         svg_document.add(svg_document.rect(insert = (10, i*150 + 10),
@@ -30,12 +39,19 @@ for i in range(len(listEntites)):
                                            stroke_width = "1",
                                            stroke = "black",
                                            fill = "rgb(209, 250, 46)"))
+        entityCoords = {
+            "nomEntite": listEntites[i],
+            "coordX": 10,
+            "coordY": i*150 + 10
+        }
+        entityCoordsList.append(entityCoords)            
 
         svg_document.add(svg_document.rect(insert = (10, i*150 + 10),
                                            size = ("150px", "26px"),
                                            stroke_width = "1",
                                            stroke = "black",
                                            fill = "rgb(209, 250, 46)"))
+        
 
         # Affichage du nom de l'entité
         svg_document.add(svg_document.text(listEntites[i],
@@ -63,11 +79,20 @@ for i in range(len(listEntites)):
                                            stroke = "black",
                                            fill = "rgb(209, 250, 46)"))
 
+        entityCoords = {
+            "nomEntite": listEntites[i],
+            "coordX": 400,
+            "coordY": (i - 1)*150 + 10
+        }
+        entityCoordsList.append(entityCoords)
+
         svg_document.add(svg_document.rect(insert = (400, (i - 1)*150 + 10),
                                            size = ("150px", "26px"),
                                            stroke_width = "1",
                                            stroke = "black",
                                            fill = "rgb(209, 250, 46)"))
+
+        
 
         # Affichage du nom de l'entité
         svg_document.add(svg_document.text(listEntites[i],
@@ -88,10 +113,25 @@ for i in range(len(listEntites)):
                 font_weight="bold")
             )
 
-svg_document.add(svg_document.line(start=(10, 10), end=(50, 50)))
+        svg_document.add(svg_document.line(
+            (550,  (i - 1)*150 + 10),
+            (100, 100),
+            stroke_width = "2",
+            stroke="rgb(15, 15, 15)"))
+
+    # Recuperations des differentes associations
+    nbAssocs = len(myJsonData[i][listEntites[i]]['relations']['associations'])
+    for j in range(nbAssocs):
+        # for key in myJsonData[i][listEntites[i]]['relations']['associations'][j].keys():
+        #     listAssocs.append(key)
+        #     print(key)
+        nomAutreEntite = myJsonData[i][listEntites[i]]['relations']['associations'][j]["nomAutreEntite"]
+        for k in range(len(entityCoordsList)):
+            if entityCoordsList[k]['nomEntite'] == nomAutreEntite:
+                print('Yeeeeeeeaah')
 # ---------------
 
-
+# print(entityCoordsList)
 # print(svg_document.tostring())
 
 svg_document.save()
